5b6bc7da936564dcbad946b7dfa1ba768fe4e83f
Sam Nolan <samnolan555@gmail.com>
2016-08-15 18:10:29 +1000

    Add several fixes for the new API
    The resources was not updated for the new
    API in lua, this makes the neccesary changes

--- script/lua/creatures/Rat.lua
+++ script/lua/creatures/Rat.lua
@@ -1,10 +1,7 @@
-States.newProximityState("Close", 4);
+newProximityState("Close", 4);
 
 function isClose(target)
-	pX,pY = target:getPosition();
-	rX, rY = getPosition();
-	moveX = (rX-pX) * 2;
-	moveY = (rY-pY) * 2;
-	moveOn(rX + moveX, rY+moveY);
-	rat = Creature.spawnCreature("Big Rat", 190, 205);
+	playerPos = target.getPosition()
+	ratPos = getPosition()
+	moveOn(((ratPos - playerPos) * 2) + ratPos);
 end
